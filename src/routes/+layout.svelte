d465aa7e5631c1509bf2f6b19a8117a783650dc3
AbdulGafar Olamide Ajao /vary-able <Gafarolamide.ajao@gmail.com>
2025-12-16 06:13:33 +0400

    Initial commit: SvelteKit project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="/favicon.ico" sizes="any" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		rel="preload"
+		as="style"
+		href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Poppins:wght@600;700&display=swap"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Poppins:wght@600;700&display=swap"
+		media="print"
+		onload={(e) => (((e.currentTarget as HTMLLinkElement) || e.target) as HTMLLinkElement).media = 'all'}
+	/>
+	<noscript>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Poppins:wght@600;700&display=swap"
+		/>
+	</noscript>
+</svelte:head>
+
+{@render children()}
